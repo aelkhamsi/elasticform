cbb5923a2fe731d66791b9aa07669d7162ab3b97
Achraf <achrafelkhamsi@gmail.com>
2026-02-28 10:38:39 +0100

    create form submission entity typeorm repository

---
+++ packages/core/src/infrastructures/typeorm/repositories/form-submission.repository.ts
@@ -0,0 +1,43 @@
+import { IBaseRepository } from "src/repositories/form.repository";
+import { FormSubmissionEntity } from "../entities/form-submission.entity";
+import { DataSource, Repository } from "typeorm";
+
+export class FormSubmissionRepository
+  implements IBaseRepository<FormSubmissionEntity, number>
+{
+  private readonly formSubmissionRepository: Repository<FormSubmissionEntity>;
+
+  constructor(dataSource: DataSource) {
+    this.formSubmissionRepository =
+      dataSource.getRepository(FormSubmissionEntity);
+  }
+
+  findAll(): Promise<FormSubmissionEntity[]> {
+    return this.formSubmissionRepository.find();
+  }
+
+  findById(id: number): Promise<FormSubmissionEntity | null> {
+    return this.formSubmissionRepository.findOneBy({ id });
+  }
+
+  create(data: Partial<FormSubmissionEntity>): Promise<FormSubmissionEntity> {
+    const entity = this.formSubmissionRepository.create(data);
+    return this.formSubmissionRepository.save(entity);
+  }
+
+  async update(
+    id: number,
+    data: Partial<FormSubmissionEntity>,
+  ): Promise<FormSubmissionEntity | null> {
+    const existing = await this.findById(id);
+    if (!existing) return null;
+
+    const entity = this.formSubmissionRepository.merge(existing, data);
+    return this.formSubmissionRepository.save(entity);
+  }
+
+  async delete(id: number): Promise<boolean> {
+    const result = await this.formSubmissionRepository.delete(id);
+    return (result.affected ?? 0) > 0;
+  }
+}
